f3d95600ff914c19b46a0003e001856ebe900f70
ath31st <ath31st@yandex.ru>
2024-01-22 19:17:16 +0300

    update Constants and Converter

--- src/main/java/dec/gen/Constants.java
+++ src/main/java/dec/gen/Constants.java
@@ -2,9 +2,11 @@ package dec.gen;
 
 public enum Constants {
   MAX_BIT_LENGTH(96),
-  MAX_SCALE(28);
+  MAX_SCALE(28),
+  NEG_SIGN(1),
+  POS_SIGN(0);
 
-  private Integer value;
+  private final Integer value;
 
   Constants(Integer value) {
     this.value = value;
--- src/main/java/dec/gen/Converter.java
+++ src/main/java/dec/gen/Converter.java
@@ -1,6 +1,10 @@
 package dec.gen;
 
+import static dec.gen.Constants.NEG_SIGN;
+
 import java.math.BigDecimal;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class Converter {
   public BigDecimal fromStrToDec(String strValue) {
@@ -9,5 +13,19 @@ public class Converter {
 
   public String fromDecToStr(BigDecimal bd) {
     return bd.toPlainString();
+  }
+
+  public S21Decimal fromDecToS21Dec(BigDecimal bd) {
+    S21Decimal d = new S21Decimal();
+    d.setScaleInBit(bd.scale());
+    if (bd.compareTo(BigDecimal.ZERO) < 0) {
+      d.setSignInBit(NEG_SIGN.getValue());
+    }
+    byte bytes[] = bd.unscaledValue().toByteArray();
+    ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
+    for (int i = 0; i < bytes.length; i++) {
+      buffer.put(bytes[i]);
+    }
+    return d;
   }
 }
