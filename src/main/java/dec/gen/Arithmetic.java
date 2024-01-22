2a8e3bf46dd067058d5ed14ab211efdc44cf96a7
ath31st <ath31st@yandex.ru>
2024-01-22 13:47:05 +0300

    add sub, div, mul methods

--- src/main/java/dec/gen/Arithmetic.java
+++ src/main/java/dec/gen/Arithmetic.java
@@ -1,10 +1,28 @@
 package dec.gen;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Arithmetic {
 
   public void add(BigDecimal bd1, BigDecimal bd2) {
     System.out.println(bd1.add(bd2).toPlainString());
+  }
+
+  public void sub(BigDecimal bd1, BigDecimal bd2) {
+    System.out.println(bd1.subtract(bd2).toPlainString());
+  }
+
+  public void mul(BigDecimal bd1, BigDecimal bd2) {
+    System.out.println(bd1.multiply(bd2).toPlainString());
+  }
+
+  public void div(BigDecimal bd1, BigDecimal bd2) {
+    if (bd2.compareTo(BigDecimal.ZERO) != 0) {
+      BigDecimal result = bd1.divide(bd2, Constants.MAX_SCALE.getValue(), RoundingMode.HALF_EVEN);
+      System.out.println(result.toPlainString());
+    } else {
+      System.out.println("Деление на ноль невозможно.");
+    }
   }
 }
