716e193442498a51d57bf0b59fa16c4fb0d7c895
ath31st <ath31st@yandex.ru>
2024-01-27 09:34:41 +0300

    add more methods for FileHandler

--- src/main/java/dec/starter/constant/FileHandlerConstants.java
+++ src/main/java/dec/starter/constant/FileHandlerConstants.java
@@ -8,10 +8,13 @@ public enum FileHandlerConstants {
   UNIX_PATH("/Documents/s21_decimal_starter/"),
   SAVE_SUCCESS("Данные успешно записаны в файл."),
   SAVE_UNSUCCESSFUL("Сохранение в файл не было произведено по причине: "),
+  DIRECTORY_ABSENT("Папка для сохранения файлов отсутствует, возможно, нет прав для ее создания."),
   WIN("win"),
   NUX("nux"),
   MAC("mac"),
-  NIX("nix");
+  NIX("nix"),
+  DELETE_SUCCESS("Удаление папки прошло успешно."),
+  DELETE_UNSUCCESSFUL("Удаление папки не удалось, возможно, нет прав для удаления.");
   private final String value;
 
   FileHandlerConstants(String value) {
--- src/main/java/dec/starter/handler/FileHandler.java
+++ src/main/java/dec/starter/handler/FileHandler.java
@@ -7,9 +7,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FileHandler {
-  private final String userHomePath;
+  private final String saveDirectory;
   private final OutputManager outputManager;
 
   public FileHandler(OutputManager outputManager) {
@@ -17,31 +19,56 @@ public class FileHandler {
     String osName = System.getProperty(OS_NAME.getValue()).toLowerCase();
 
     if (osName.contains(WIN.getValue())) {
-      userHomePath = System.getProperty(USER_HOME.getValue()) + WIN_PATH.getValue();
+      saveDirectory = System.getProperty(USER_HOME.getValue()) + WIN_PATH.getValue();
     } else if (osName.contains(NIX.getValue())
         || osName.contains(NUX.getValue())
         || osName.contains(MAC.getValue())) {
-      userHomePath = System.getProperty(USER_HOME.getValue()) + UNIX_PATH.getValue();
+      saveDirectory = System.getProperty(USER_HOME.getValue()) + UNIX_PATH.getValue();
     } else {
-      userHomePath = System.getProperty(USER_DIR.getValue());
+      saveDirectory = System.getProperty(USER_DIR.getValue());
     }
   }
 
   public void saveContentToFile(String fileName, String content) {
-    File directory = new File(userHomePath);
+    File directory = new File(saveDirectory);
     boolean success = false;
     if (!directory.exists()) {
       success = directory.mkdir();
     }
     if (success) {
-      try (BufferedWriter writer = new BufferedWriter(new FileWriter(userHomePath + fileName))) {
+      try (BufferedWriter writer = new BufferedWriter(new FileWriter(saveDirectory + fileName))) {
         writer.write(content);
         outputManager.consolePrint(SAVE_SUCCESS.getValue());
       } catch (IOException e) {
         outputManager.consolePrint(SAVE_UNSUCCESSFUL.getValue()
             + e.getMessage());
       }
+    } else {
+      outputManager.consolePrint(DIRECTORY_ABSENT.getValue());
+    }
+  }
+
+  public void deleteFilesAndDirectory() {
+    File directory = new File(saveDirectory);
+
+    if (directory.exists()) {
+      File[] files = directory.listFiles();
+
+      if (files != null) {
+        for (File file : files) {
+          if (file.isFile()) {
+            file.delete();
+          }
+        }
+      }
+      try {
+        Files.delete(Path.of(saveDirectory));
+        outputManager.consolePrint(DELETE_SUCCESS.getValue());
+      } catch (IOException e) {
+        outputManager.consolePrint(DELETE_UNSUCCESSFUL.getValue());
+      }
+    } else {
+      outputManager.consolePrint(DIRECTORY_ABSENT.getValue());
     }
   }
 }
-
