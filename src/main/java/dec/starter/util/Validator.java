5870d6e3b248434b4a19099337369d384e4aaf27
ath31st <ath31st@yandex.ru>
2024-02-02 14:52:29 +0300

    add java doc for Validator

--- src/main/java/dec/starter/util/Validator.java
+++ src/main/java/dec/starter/util/Validator.java
@@ -5,13 +5,23 @@ import static dec.starter.constant.ArithmeticConstants.MAX_SCALE;
 
 import java.math.BigDecimal;
 
+/**
+ * The Validator class provides methods to validate properties of BigDecimal numbers.
+ * It checks for conditions such as scale, precision, and range to determine the validity
+ * of a given BigDecimal number based on predefined constants and thresholds.
+ */
 public class Validator {
   private final BigDecimal maxS21Decimal = new BigDecimal("79228162514264337593543950335");
   private final BigDecimal minS21Decimal = new BigDecimal("-79228162514264337593543950335");
 
-  //  0 - OK
-  //  1 - the number is too large or equal to infinity
-  //  2 - the number is too small or equal to negative infinity
+  /**
+   * Checks the validity of a BigDecimal number based on scale, precision, and range.
+   *
+   * @param bd The BigDecimal number to be validated.
+   * @return Zero if the BigDecimal is valid,
+   *     1 if the number is too large or equal to positive infinity,
+   *     2 if the number is too small or equal to negative infinity.
+   */
   public int checkBigDecimal(BigDecimal bd) {
     int checkRes = 0;
     int scale = bd.scale();
