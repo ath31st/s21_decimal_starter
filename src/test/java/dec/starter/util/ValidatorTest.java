0b29007e527133a41ab7695398fd463d23443771
ath31st <ath31st@yandex.ru>
2024-02-17 19:40:29 +0300

    refactoring ValidatorTest

--- src/test/java/dec/starter/util/ValidatorTest.java
+++ src/test/java/dec/starter/util/ValidatorTest.java
@@ -6,10 +6,10 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 class ValidatorTest {
+  private final Validator validator = new Validator();
 
   @Test
   void testValidBigDecimal() {
-    Validator validator = new Validator();
     BigDecimal validBigDecimal = new BigDecimal("123.45");
     int result = validator.checkBigDecimal(validBigDecimal);
     assertEquals(0, result);
@@ -17,7 +17,6 @@ class ValidatorTest {
 
   @Test
   void testBigDecimalTooSmall() {
-    Validator validator = new Validator();
     BigDecimal tooSmallBigDecimal = new BigDecimal("-79228162514264337593543950336");
     int result = validator.checkBigDecimal(tooSmallBigDecimal);
     assertEquals(2, result);
@@ -25,7 +24,6 @@ class ValidatorTest {
 
   @Test
   void testBigDecimalTooLarge() {
-    Validator validator = new Validator();
     BigDecimal tooLargeBigDecimal = new BigDecimal("79228162514264337593543950336");
     int result = validator.checkBigDecimal(tooLargeBigDecimal);
     assertEquals(1, result);
@@ -33,7 +31,6 @@ class ValidatorTest {
 
   @Test
   void testValidMaxS21Decimal() {
-    Validator validator = new Validator();
     BigDecimal maxS21Decimal = new BigDecimal("79228162514264337593543950335");
     int result = validator.checkBigDecimal(maxS21Decimal);
     assertEquals(0, result);
@@ -41,7 +38,6 @@ class ValidatorTest {
 
   @Test
   void testValidMinS21Decimal() {
-    Validator validator = new Validator();
     BigDecimal minS21Decimal = new BigDecimal("-79228162514264337593543950335");
     int result = validator.checkBigDecimal(minS21Decimal);
     assertEquals(0, result);
@@ -49,7 +45,6 @@ class ValidatorTest {
 
   @Test
   void testBigScaleS21Decimal() {
-    Validator validator = new Validator();
     BigDecimal bigScaleS21Decimal = new BigDecimal("7.9228162514264337593543950335");
     int result = validator.checkBigDecimal(bigScaleS21Decimal);
     assertEquals(0, result);
@@ -57,7 +52,6 @@ class ValidatorTest {
 
   @Test
   void testInvalidBigScaleS21Decimal() {
-    Validator validator = new Validator();
     BigDecimal bigScaleS21Decimal = new BigDecimal("0.79228162514264337593543950335");
     int result = validator.checkBigDecimal(bigScaleS21Decimal);
     assertEquals(2, result);
@@ -65,7 +59,6 @@ class ValidatorTest {
 
   @Test
   void testInvalidVaxPrecisionS21Decimal() {
-    Validator validator = new Validator();
     BigDecimal bigPrecisionS21Decimal = new BigDecimal("7922816251426433759354395033599");
     int result = validator.checkBigDecimal(bigPrecisionS21Decimal);
     assertEquals(1, result);
